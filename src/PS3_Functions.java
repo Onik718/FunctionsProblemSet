d341d4ace0ce677f3f814469045092643f9f9a0d
jessehartloff <hartloff@buffalo.edu>
2017-02-09 16:02:08 -0500

    initial commit

---
+++ src/PS3_Functions.java
@@ -0,0 +1,96 @@
+public class PS3_Functions {
+
+
+    static String Q1(String name){
+
+        // return "Hello <name>!"
+
+        return "";
+    }
+
+    static int Q2(String input){
+
+        // return the length of the input
+
+        return 0;
+    }
+
+    static String Q3(String input){
+
+        // return the input in all lowercase letters
+        return "";
+    }
+
+    static String Q4(String input){
+
+        // return the input in all uppercase letters
+        return "";
+    }
+
+    static String Q5(String input){
+
+        // return the reverse of the string
+        return "";
+    }
+
+    static int Q6(int input){
+        // return input time 2
+        return 0;
+    }
+
+    static double Q7(double input){
+        // return input divided by 7
+        return 0.0;
+    }
+
+    static double Q8(int input){
+        // return input times 1.05
+        return 0.0;
+    }
+
+    static double Q9(double input){
+        // return the cosine of input. The input is given in radians
+        return 0.0;
+    }
+
+    static boolean Q10(int input){
+        // return true if the input is 21 or over, false otherwise
+        return false;
+    }
+
+    static int Q11(int input1, int input2){
+        // return input1 minus input2
+        return 0;
+    }
+
+    static String Q12(String input, int times){
+        // return the input concatenated times number of times
+        return "";
+    }
+
+    static double Q13(double input1, double input2){
+        // return input1 raised to the input2 power
+        return 0.0;
+    }
+
+    static double Q14(String input, double number){
+        // return the length of the input string times number
+        return 0.0;
+    }
+
+    static String Q15(boolean bool, String input){
+        // if bool is true, return the input in all lowercase letters
+        // if bool is false, return the input in all uppercase letters
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+        Q1("Dave");
+
+        // You can use this main method to test your code without having to submit.
+        // This main method will not be graded
+
+    }
+
+}
